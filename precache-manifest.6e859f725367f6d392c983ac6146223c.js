7fba9512d677195dbf2ac51703c7a4fe53a4c84f
Bortko Daria <138530655+BortkoDaria@users.noreply.github.com>
2025-10-03 04:45:01 +0300

    Add files via upload

---
+++ precache-manifest.6e859f725367f6d392c983ac6146223c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122c52878a367bf9571c9514647fae51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53988719253fe32e6799",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "53988719253fe32e6799",
+    "url": "/static/js/main.764eb273.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
