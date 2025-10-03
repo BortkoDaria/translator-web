e366a0a751c8640f82191e0cf24072f487858450
Bortko Daria <138530655+BortkoDaria@users.noreply.github.com>
2025-10-03 04:16:51 +0300

    Add files via upload

---
+++ precache-manifest.90473eb9b879d7999ced43edce9a0214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f89c047ddea85c8ccda075bd51aaf0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798e61b416bd3c66f2d5",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "798e61b416bd3c66f2d5",
+    "url": "/static/js/main.bf4cd408.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
